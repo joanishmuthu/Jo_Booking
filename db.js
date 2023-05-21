6507de496c588442b8b7ee33470582b54b4c93f1
Joanish Muthu <52238376+joanishmuthu@users.noreply.github.com>
2023-05-21 20:38:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb+srv://joanish:29101999@shey.pnu20lr.mongodb.net/shey_rooms'
+var mongoURL = 'your url'
 
 mongoose.connect(mongoURL,{useUnifiedTopology : true, useNewUrlParser: true})
 
